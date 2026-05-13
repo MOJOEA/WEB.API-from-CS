bb1f3fda07cfc67888774a93cdb3bdd5c11ec0c2
MOJOEA <DevineTYpor@gmail.com>
2026-05-13 10:35:58 +0700

    set server

---
+++ node-expres-mysql/server.ts
@@ -0,0 +1,8 @@
+import http = require("http");
+
+const port = process.env.PORT || 3000;
+const server = http.createServer();
+
+server.listen(port, () => {
+  console.log("Server is started");
+});
