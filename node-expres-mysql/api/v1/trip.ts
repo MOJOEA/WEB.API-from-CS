b9f261807bde7c00b934a41cd56f9e6ae2577524
MOJOEA <DevineTYpor@gmail.com>
2026-05-13 13:03:46 +0700

    .

--- node-expres-mysql/api/v1/trip.ts
+++ node-expres-mysql/api/v1/trip.ts
@@ -1,21 +1,86 @@
-import express, { json } from "express";
-import { send } from "process";
+import express from "express";
+import { conn } from "../../database/connections";
+import { TripPostRequest } from "../../model/trip";
+import mysql = require("mysql2");
+
 export const router = express.Router(); 
 
 router.get("/", (req, res) => {
-  conn.query('select * from trip', (err, result, fields)=>{
+  conn.query('select * from trip', (err, result) => {
     res.json(result);
   });
 });
 
 router.get("/:id", (req, res) => {
-  res
-    .send("Get in trip.ts id: " + req.params.id);
+  const { id } = req.params;
+  conn.query("select * from trip where idx = ?" , [id], (err, result) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.json(result);
+  });
+});
+
+router.get("/search/fields", (req, res) => {
+  conn.query(
+    "select * from trip where (idx IS NULL OR idx = ?) OR (name IS NULL OR name like ?)",
+    [ req.query.id, "%" + req.query.name + "%"],
+    (err, result) => {
+      if (err) return res.status(500).json({ error: err.message });
+      res.json(result);
+    }
+  );
 });
 
 router.post("/", (req, res) => {
-  let body = req.body;
-  res
-    .status(201)
-    .json({ text: "Get in trip.ts body: " + JSON.stringify(body) });
+  let trip: TripPostRequest = req.body;
+  let sql =
+    "INSERT INTO `trip`(`name`, `country`, `destinationid`, `coverimage`, `detail`, `price`, `duration`) VALUES (?,?,?,?,?,?,?)";
+  sql = mysql.format(sql, [
+    trip.name,
+    trip.country,
+    trip.destinationid,
+    trip.coverimage,
+    trip.detail,
+    trip.price,
+    trip.duration,
+  ]);
+
+  conn.query(sql, (err, result: any) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res
+      .status(201)
+      .json({ affected_row: result.affectedRows, last_idx: result.insertId });
+  });
+});
+
+router.put("/:id", (req, res) => { 
+  let { id } = req.params;
+  let trip: TripPostRequest = req.body;
+  let sql =
+    "UPDATE `trip` SET `name`=?,`country`=?,`destinationid`=?,`coverimage`=?,`detail`=?,`price`=?,`duration`=? WHERE idx = ?";
+  sql = mysql.format(sql, [
+    trip.name,
+    trip.country,
+    trip.destinationid,
+    trip.coverimage,
+    trip.detail,
+    trip.price,
+    trip.duration,
+    id
+  ]);
+  conn.query(sql, (err, result) => {
+    if (err) throw err;
+    res
+      .status(201)
+      .json({ affected_row: result });
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  let id = +req.params.id;
+  conn.query("delete from trip where idx = ?", [id], (err, result) => {
+     if (err) throw err;
+     res
+       .status(200)
+       .json({ affected_row: result });
+  });
 });
--- node-expres-mysql/database/connections.ts
+++ node-expres-mysql/database/connections.ts
@@ -1,9 +1,10 @@
-import mysql from "mysql";
+import mysql = require("mysql2"); 
 
-export const conn = mysql.createPool({
+export const conn: mysql.Pool = mysql.createPool({
   connectionLimit: 10,
-  host: "localhost",
+  host: "127.0.0.1", 
   user: "tripbooking",
   password: "tripbooking@csmsu",
   database: "tripbooking",
+  port: 3306 
 });
